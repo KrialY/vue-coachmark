7fa51ee41b4ba6abdbf3b6c3575858a3561860de
yuankailong <947102061@qq.com>
2024-07-22 21:32:53 +0800

    build: config auto generate d.ts

--- src/components/CoachMark/CoachMark.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@/components/CoachMark/CoachMark.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/components/CoachMark'),
+      entry: path.resolve(__dirname, 'src/components/CoachMark/index.ts'),
       name: 'CoachMark',
       fileName: (format) => `index.${format}.js`
     },
@@ -23,7 +24,13 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    cssInjectedByJsPlugin()
+    cssInjectedByJsPlugin(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+      outDir: 'dist/types',
+      insertTypesEntry: true,
+      cleanVueFileName: true
+    })
   ],
   resolve: {
     alias: {
